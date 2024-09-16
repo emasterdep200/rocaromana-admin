34c6b2f711dfe2fb0083050c09e730fe14392303
Luis Toscano <altrixdao@protonmail.com>
2024-09-16 01:39:54 -0500

    fgfdghfdh

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -41,8 +41,8 @@ class VentaController extends Controller{
 
 
             $customers = Customer::where(['is_asesor' => 0])->whereIn('city', $ciudadeCodes)->count();
-            $asesores  = Asesor::where(['ciudad' => Auth::user()->zone])->whereIn('ciudad', $ciudadeCodes)->count();
-            $ventas    = Ventas::where(['city' => Auth::user()->zone])->whereIn('city', $ciudadeCodes)->with('package')->get();
+            $asesores  = Asesor::whereIn('ciudad', $ciudadeCodes)->count();
+            $ventas    = Ventas::whereIn('city', $ciudadeCodes)->with('package')->get();
         }else{
             $customers = Customer::where(['is_asesor' => 0])->count();
             $asesores  = Asesor::all()->count();
