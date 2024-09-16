9752d7a21fa39d483bb9a4c06791ea29d864e5d8
Luis Toscano <altrixdao@protonmail.com>
2024-09-16 01:16:40 -0500

    dgfsghfghfg

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -33,7 +33,7 @@ class VentaController extends Controller{
         if(Auth::user()->zone != NULL){
             $zona = Zonas::where(['id' => Auth::user()->zone])->first();
 
-            Log::info(print_r($zona->ciudades, true));  
+            Log::info(Auth::user()->zone);  
 
 
             $customers = Customer::where(['is_asesor' => 0, 'city' => Auth::user()->zone])->count();
