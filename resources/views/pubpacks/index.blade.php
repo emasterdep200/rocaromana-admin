c69ccc4ad65b57a771e8550979c6639686819e6f
Luis Toscano <altrixdao@protonmail.com>
2024-09-30 22:12:43 -0500

    create package

--- resources/views/pubpacks/index.blade.php
+++ resources/views/pubpacks/index.blade.php
@@ -25,7 +25,7 @@
             <div class="card-header">
                 <div class="col-sm-12 d-flex justify-content-end">
 
-                    <a class="btn btn-primary me-1 mb-1" data-bs-toggle="modal" data-bs-target="#addAnuncio">Agregar Anuncio</a>
+                    <a class="btn btn-primary me-1 mb-1" data-bs-toggle="modal" data-bs-target="#addPack">Agregar Paquete</a>
 
                 </div>
             </div>
@@ -64,45 +64,35 @@
 
 
     <!-- EDIT USER MODEL MODEL -->
-    <div id="addAnuncio" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+    <div id="addPack" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
         aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="myModalLabel1">DETALLE DEL ANUNCIO</h5>
+                    <h5 class="modal-title" id="myModalLabel1">DETALLE DEL PAQUETE</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
 
-                    <form method="post" action="{{ url('anuncio_create') }}" enctype="multipart/form-data">
+                    <form method="post" action="{{ url('pubpackage_create') }}" enctype="multipart/form-data">
                     @csrf
 
                         <!-- Campo Título -->
                         <div class="mb-3">
-                            <label for="titulo" class="form-label">Título</label>
-                            <input type="text" class="form-control" name="titulo" id="titulo" placeholder="Ingresa el título" required>
+                            <label for="titulo" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" required>
                         </div>
 
                         <!-- Campo Imagen -->
                         <div class="mb-3">
-                            <label for="imagen" class="form-label">Imagen</label>
-                            <input type="file" class="form-control" name="image" id="imagen" accept="image/*" required>
+                            <label for="imagen" class="form-label">Precio</label>
+                            <input type="text" class="form-control" name="price" id="precio" required>
                         </div>
 
                         <!-- Campo Link -->
                         <div class="mb-3">
-                            <label for="link" class="form-label">Link</label>
-                            <input type="url" class="form-control" name="link" id="link" placeholder="Ingresa el link" required>
-                        </div>
-
-                        <!-- Campo Estado -->
-                        <div class="mb-3">
-                            <label for="estado" class="form-label">Estado</label>
-                            <select class="form-select" id="estado" name="estado" required>
-                                <option value="">Selecciona un estado</option>
-                                <option value="activo">Activo</option>
-                                <option value="inactivo">Inactivo</option>
-                            </select>
+                            <label for="link" class="form-label">Valido por (dias)</label>
+                            <input type="text" class="form-control" name="days_valid" id="days_valid" required>
                         </div>
 
                         <!-- Botón Enviar -->
